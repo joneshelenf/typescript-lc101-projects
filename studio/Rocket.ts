dd96554109eed2952f55e8a79f114aef24d40a50
Helen Jones <helenfrancesjones@gmail.com>
2023-03-10 16:42:57 -0600

    finished methods

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,5 +1,6 @@
 import { Cargo } from './Cargo';
 import { Astronaut } from './Astronaut';
+import { Payload } from './Payload';
 
 export class Rocket {
 
@@ -11,5 +12,29 @@ export class Rocket {
     constructor (name: string, totalCapacityKg: number) {
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]): number {
+        let sum: number = 0;
+        for (let i=0; i < items.length; i++){
+            sum += items[i].massKg;
+        }
+        return sum;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    }
+
+    canAdd(item: Payload): boolean {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
     }
 }
